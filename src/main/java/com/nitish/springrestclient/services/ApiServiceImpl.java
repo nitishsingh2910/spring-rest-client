ab298abaf3f4b178db8242084ab41893c71b549b
Nitish Singh <nsingh07@syr.edu>
2020-07-03 14:11:51 -0400

    Added URI component builder

--- src/main/java/com/nitish/springrestclient/services/ApiServiceImpl.java
+++ src/main/java/com/nitish/springrestclient/services/ApiServiceImpl.java
@@ -2,8 +2,10 @@ package com.nitish.springrestclient.services;
 
 import com.nitish.api.domain.User;
 import com.nitish.api.domain.UserData;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import java.util.List;
 
@@ -11,14 +13,19 @@ import java.util.List;
 public class ApiServiceImpl implements ApiService {
 
     private RestTemplate restTemplate;
+    private String api_url;
 
-    public ApiServiceImpl(RestTemplate restTemplate) {
+    public ApiServiceImpl(RestTemplate restTemplate, @Value("${api.url}") String api_url) {
         this.restTemplate = restTemplate;
+        this.api_url = api_url;
     }
 
     @Override
     public List<User> getUsers(Integer limit) {
-        UserData userData = restTemplate.getForObject("http://apifaketory.com/api/user?limit="+limit, UserData.class);
+
+        UriComponentsBuilder uriBuilder = UriComponentsBuilder.fromUriString(api_url).queryParam("limit", limit);
+
+        UserData userData = restTemplate.getForObject(uriBuilder.toString(), UserData.class);
         return userData.getData();
     }
 }
